68ffd7440bded0724856997df88a8dd5d811a91d
Kevin Loi <kevin@murj.com>
2020-11-17 17:20:55 -0800

    cleanup on aisle 5

--- js/main.js
+++ js/main.js
@@ -1,68 +1,32 @@
+ $(document).ready(function() {
+            
+    // What to do on load
+    // 1. scroll to top if it isn't scrolled to top yet
+    // 2. initialize glider for carousel
+    window.addEventListener('load', function(){
 
-$(document).ready(function () {
+//        if ( $(this).scrollTop() > 0 || !$(this).scrollTop() )
+//            $(window).scrollTo(0);
 
-
-    let $btns = $('.project-area .button-group button');
-
-
-    $btns.click(function (e) {
-
-        $('.project-area .button-group button').removeClass('active');
-        e.target.classList.add('active');
-
-        let selector = $(e.target).attr('data-filter');
-        $('.project-area .grid').isotope({
-            filter: selector
+        $(".owl-carousel").owlCarousel({
+            items: 1,
         });
-
-        return false;
-    })
-
-    $('.project-area .button-group #btn1').trigger('click');
-
-    $('.project-area .grid .test-popup-link').magnificPopup({
-        type: 'image',
-        gallery: { enabled: true }
     });
 
-
-    // Owl-carousel
-
-    $('.site-main .about-area .owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: true,
-        dots: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            560: {
-                items: 2
-            }
+    // What happens when you scroll
+    // when you scroll past the header, navbar becomes fixed. 
+    // when you scroll up to banner area, navbar becomes relative
+    // adding/removing body padding-top removes the choppiness of displaying and hiding navbar
+    $(window).scroll(function() {
+        var nav = $('.main-menu');
+        var banner_height = $('#banner-area').height();
+        if( $(this).scrollTop() >= banner_height ) {
+            nav.addClass('fixed-top bg-light');
+            $('body').css('paddingTop', nav.height());
+        } else {
+            nav.removeClass('fixed-top bg-light');
+            $('body').css('padding', 0);
         }
-    })
-
-    // sticky navigation menu
-
-    let nav_offset_top = $('.header_area').height() + 50;
-
-    function navbarFixed() {
-        if ($('.header_area').length) {
-            $(window).scroll(function () {
-                let scroll = $(window).scrollTop();
-                if (scroll >= nav_offset_top) {
-                    $('.header_area .main-menu').addClass('navbar_fixed');
-                } else {
-                    $('.header_area .main-menu').removeClass('navbar_fixed');
-                }
-            })
-        }
-    }
-
-    navbarFixed();
-
-    $(document).ready(function() {
-        $(".dropdown-toggle").dropdown();
-    });
+     });
 
 });
